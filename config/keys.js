de3e1245a85ef5cbf3957206a25ea2b6d0ce484f
Aastha05 <aastharanjan0507@gmail.com>
2024-05-29 18:09:54 +0530

    google client id

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     mongoURI: 'YOUR_API_URL',
     secretOrKey: 'YOUR_SECRET_KEY',
-    // googleClientID: 'YOUR_GOOGLE_CLIENT_ID',
-    // googleClientSecret: 'YOUR_CLIENT_SECRET',
+    googleClientID: 'YOUR_GOOGLE_CLIENT_ID',
+    googleClientSecret: 'YOUR_CLIENT_SECRET_KEY',
     googleCallbackUrl: 'http://localhost:5000/api/auth/google/callback'
   };
   
