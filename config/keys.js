e2e3c67b4eae189f0cd7115f342720e46979b8f1
Aastha05 <aastharanjan0507@gmail.com>
2024-05-29 18:06:12 +0530

    removed secret key

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     mongoURI: 'YOUR_API_URL',
     secretOrKey: 'YOUR_SECRET_KEY',
     googleClientID: '303766447057-7cbjjvpqf4veokcr9hqu0qhu3ibi3k91.apps.googleusercontent.com',
-    googleClientSecret: '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',
+    googleClientSecret: 'YOUR_CLIENT_SECRET',
     googleCallbackUrl: 'http://localhost:5000/api/auth/google/callback'
   };
   
